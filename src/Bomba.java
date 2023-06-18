d2931324dd86d7f1d9fa972ceae056987528f908
Bruno <brunoduartrosa090@gmail.com>
2023-06-18 19:23:31 -0300

    Adicionando a imagem de bomba

---
+++ src/Bomba.java
@@ -0,0 +1,14 @@
+
+
+public class Bomba extends ElementoBasico {
+
+
+    public Bomba(String id, int linInicial, int colInicial, Tabuleiro tabuleiro) {
+        super(id, "minesweeper.png", linInicial, colInicial, tabuleiro);
+    }
+
+    @Override
+    public void acao(ElementoBasico outro) {
+
+    }
+}
--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -126,7 +126,7 @@ public class Tabuleiro extends JPanel {
             case ' ':
                 return new Fundo("Fundo", lin, col, this);
             case '-':
-                return new Eca("Dica", lin, col, this);
+                return new Bomba("Dica", lin, col, this);
             case '?':
                 return new Pista("Pista", r.nextInt(15), lin, col, this);
             case '^':
